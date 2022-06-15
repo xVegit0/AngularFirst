541f4f2df39217a0e16ac1d791e1e86b89ab5f82
Konrad J <konrad.jakielaszek@gmail.com>
2022-06-15 14:42:10 +0200

    Misja 19

--- src/app/space/pilot-room/pilot-room.component.spec.ts
+++ src/app/space/pilot-room/pilot-room.component.spec.ts
@@ -1,23 +1,106 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Pilot } from '../pilot';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
 import { PilotRoomComponent } from './pilot-room.component';
+import { PilotService } from '../pilot.service';
+import { By } from '@angular/platform-browser';
+@Component({
+	selector: 'app-pilot',
+	template: 'pilot {{pilot.firstName}} <ng-content></ng-content>',
+})
+class PilotMockComponent {
+	@Input() pilot!: Pilot;
+}
+class PilotServiceMock {
+	getPilots() {}
+}
 
-describe('PilotRoomComponent', () => {
-  let component: PilotRoomComponent;
-  let fixture: ComponentFixture<PilotRoomComponent>;
+fdescribe('PilotRoomComponent', () => {
+	let component: PilotRoomComponent;
+	let fixture: ComponentFixture<PilotRoomComponent>;
+	let pilotService: PilotService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PilotRoomComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [PilotRoomComponent, PilotMockComponent],
+			providers: [{ provide: PilotService, useClass: PilotServiceMock }],
+		}).compileComponents();
+	});
 
-    fixture = TestBed.createComponent(PilotRoomComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		pilotService = TestBed.inject(PilotService);
+		fixture = TestBed.createComponent(PilotRoomComponent);
+		component = fixture.componentInstance;
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	describe('when pilots fetched successfully', () => {
+		let pilot: Pilot;
+
+		beforeEach(() => {
+			pilot = new Pilot({
+				id: 1,
+				firstName: 'Mike',
+				lastName: 'Tomsky',
+				imageUrl: '',
+			});
+			spyOn(pilotService, 'getPilots').and.returnValue(of([pilot]));
+			spyOn(component.selected, 'emit');
+			fixture.detectChanges();
+		});
+
+		it('should display pilots', () => {
+			expect(fixture.nativeElement.textContent).toContain('Mike');
+		});
+
+		describe('when pilot is being selected', () => {
+			beforeEach(() => {
+				const selectButton = fixture.debugElement.query(
+					By.css('.select-button')
+				);
+				selectButton.nativeElement.click();
+			});
+
+			it('should store selected pilot', () => {
+				expect(component.selectedPilot).toBe(pilot);
+			});
+
+			it('should emit pilot', () => {
+				expect(component.selected.emit).toHaveBeenCalledWith(pilot);
+			});
+		});
+		describe('when pilot is being deselected', () => {
+			beforeEach(() => {
+				component.selectedPilot = pilot;
+				fixture.detectChanges();
+				const deselectButton = fixture.debugElement.query(
+					By.css('.deselect-button')
+				);
+				deselectButton.nativeElement.click();
+			});
+
+			it('should clear selection', () => {
+				expect(component.selectedPilot).toBeNull();
+			});
+
+			it('should emit null', () => {
+				expect(component.selected.emit).toHaveBeenCalledWith(null);
+			});
+		});
+	});
+	describe('when pilots fetch failed', () => {
+		beforeEach(() => {
+			spyOn(pilotService, 'getPilots').and.returnValue(
+				throwError('error')
+			);
+			spyOn(window, 'alert');
+			fixture.detectChanges();
+		});
+
+		it('should display alert with warning', () => {
+			expect(window.alert).toHaveBeenCalledWith(jasmine.any(String));
+		});
+	});
 });
